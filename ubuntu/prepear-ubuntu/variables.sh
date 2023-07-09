40074920bc866bd49524bfb8695514bea10a8e08
lukasz <lukasz@kolodziej.pro>
2023-07-09 13:08:50 +0200

    add script to prepare ubuntu

---
+++ ubuntu/prepear-ubuntu/variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Adres IP wraz z maską
+IP_ADDRESS="192.168.0.100/24"
+
+# Adres bramy domyślnej
+GATEWAY="192.168.0.1"
+
+# Serwery DNS (oddzielone spacją lub przecinkiem)
+DNS_SERVERS="8.8.8.8 8.8.4.4"
